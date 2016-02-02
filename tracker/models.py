39c17b3db36fe6927c1e49340b28655207952d11
ian luddy <iluddy@qualio.com>
2016-02-02 22:44:55 +0000

    Login pages and Session management

--- tracker/models.py
+++ tracker/models.py
@@ -1,4 +1,5 @@
 from mongoengine import *
+from datetime import datetime
 
 class User(Document):
     id = SequenceField(primary_key=True)
@@ -6,15 +7,24 @@ class User(Document):
     name = StringField()
     password = StringField()
     company = ReferenceField('Company')
+    type = StringField() # admin, leader
+
+class Member(Document):
+    id = SequenceField(primary_key=True)
+    mail = StringField(unique=True)
+    name = StringField()
+    company = ReferenceField('Company')
+
+class Event(Document):
+    id = SequenceField(primary_key=True)
+    users = ListField(ReferenceField('User'))
+    name = StringField()
+    location = StringField()
+    members = ListField(ReferenceField('Member'))
 
 class Company(Document):
     id = SequenceField(primary_key=True)
     name = StringField(unique=True)
-
-class Session(Document):
-    token = StringField(unique=True)
-    user_id = IntField()
-    company_id = IntField()
-    # expires = DateTimeField()
-
+    status = StringField(default='trialling') # trialling
+    joined = DateTimeField(default=datetime.now)
 
