e9a311361f4b0b6c2866581cdb1f88341ed65aac
Ian <ianluddy@gmail.com>
2016-04-18 17:15:34 +0100

    WSGI entry point

--- configs/wsgi.py
+++ configs/wsgi.py
@@ -1,4 +1,18 @@
-from flock import app
+import os, sys
+
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
+from flock.utils import read_config_file
+
+cfg = read_config_file('/Users/ianluddy/flock/configs/flock-develop-mac.json')
+
+import __builtin__
+__builtin__.flock_cfg = cfg
+
+from flock.app import app
+
+__builtin__.flock_app = app
+from flock import views
 
 if __name__ == '__main__':
     app.run()
--- flock/app.py
+++ flock/app.py
@@ -1,7 +1,4 @@
 import argparse
-from tornado.wsgi import WSGIContainer
-from tornado.httpserver import HTTPServer
-from tornado.ioloop import IOLoop
 from database import Database
 from constants import SECRET_KEY, SESSION_DURATION
 from flask.ext.autodoc import Autodoc
@@ -15,13 +12,14 @@ from flask import got_request_exception
 from celery import Celery
 import __builtin__
 
-# Argument parser
-arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument("-c")
-args = arg_parser.parse_args()
-
 # Config
-cfg = read_config_file(args.c)
+if not hasattr(__builtin__, 'flock_cfg'):
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-c")
+    args = arg_parser.parse_args()
+    cfg = read_config_file(args.c)
+else:
+    cfg = __builtin__.flock_cfg
 
 # Create App
 app = Flask(__name__, static_url_path='')
@@ -59,48 +57,38 @@ db = MongoEngine()
 db.init_app(app)
 db_wrapper = Database(db, cfg)
 
+# Logging
+setup_logger(cfg["logging"]["file"], cfg["logging"]["level"])
+
+# Init Rollbar
+@app.before_first_request
+def init_rollbar():
+    rollbar.init(
+        cfg["rollbar"]["token"],
+        cfg["rollbar"]["environment"],
+        root=os.path.dirname(os.path.realpath(__file__)),
+        allow_logging_basic_config=False
+    )
+    got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
+
+class CustomRequest(Request):
+    @property
+    def rollbar_person(self):
+        from flask import session
+        return {'id': session['user_id'], 'username': session['user_name'], 'email': session['email']}
+
+app.request_class = CustomRequest
+
 if __name__ == '__main__':
-
-    # TODO - not this
-    __builtin__.flock_app = app
-
-    # Logging
-    setup_logger(cfg["logging"]["file"], cfg["logging"]["level"])
-
-    # Init Rollbar
-    @app.before_first_request
-    def init_rollbar():
-        rollbar.init(
-            cfg["rollbar"]["token"],
-            cfg["rollbar"]["environment"],
-            root=os.path.dirname(os.path.realpath(__file__)),
-            allow_logging_basic_config=False
-        )
-        got_request_exception.connect(rollbar.contrib.flask.report_exception, app)
-
-    class CustomRequest(Request):
-        @property
-        def rollbar_person(self):
-            from flask import session
-            return {'id': session['user_id'], 'username': session['user_name'], 'email': session['email']}
-
-    app.request_class = CustomRequest
-
     # Create Views
+    __builtin__.flock_app = app
     from flock import views
 
-    # Run
-    app.run(cfg["web_server"]["host"])
-
     # Run with Tornado
-    # ssl = False
-    # if ssl:
-    #     http_server = HTTPServer(WSGIContainer(app), ssl_options={
-    #         "certfile": "/etc/letsencrypt/live/app.tryflock.com/cert.pem",
-    #         "keyfile": "/etc/letsencrypt/live/app.tryflock.com/privkey.pem",
-    #     })
-    # else:
-    #     http_server = HTTPServer(WSGIContainer(app))
-    #
-    # http_server.listen(cfg["web_server"]["port"], address=cfg["web_server"]["host"])
-    # IOLoop.instance().start()
+    from tornado.wsgi import WSGIContainer
+    from tornado.httpserver import HTTPServer
+    from tornado.ioloop import IOLoop
+    http_server = HTTPServer(WSGIContainer(app))
+
+    http_server.listen(cfg["web_server"]["port"], address=cfg["web_server"]["host"])
+    IOLoop.instance().start()
